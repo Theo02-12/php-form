fe1163cab6722db49746268695b862f895f5fd01
Théo Couvelard <mrcouvelard@gmail.com>
2023-05-12 15:32:17 +0200

    start

---
+++ php/includes/buttons-data.php
@@ -0,0 +1,2 @@
+<a class="btn border px-5 bg-primary text-light" href="#" name="getdata">Ajouter des données</a>
+<a class="btn border px-5 bg-secondary text-light" href="#">Plus de données</a>
---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <?php include 'includes/head.inc.html'?>
+<body>
+    <?php include 'includes/header.inc.html'?>
+    <div class="container d-flex">
+        <div class="container d-flex flex-column">
+            <a class="btn border px-5" href="#">Home</a>
+        </div>
+        <div class="container text-center">
+            <?php 
+                include 'includes/buttons-data.php'
+            ?>
+        </div>
+    </div>
+</body>
+</html>

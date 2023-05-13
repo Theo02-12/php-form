af87e0a55dba210139ff598d8c88a2485feb8fd7
Theo02-12 <mrcouvelard@gmail.com>
2023-05-13 19:31:34 +0200

    f*ck php

--- php/index.php
+++ php/index.php
@@ -1,42 +1,50 @@
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
-    <?php
+<?php
+    use function PHPSTORM_META\map;
+    include 'includes/head.inc.html';
+?>
 
-use function PHPSTORM_META\map;
-
- include 'includes/head.inc.html'?>
 <body>
-    <?php include 'includes/header.inc.html'?>
+    <?php include 'includes/header.inc.html' ?>
     <div class="container d-flex flex-column flex-md-row">
-        <div class="container d-flex flex-column mt-5 md-w-25">
-            <a class="btn border px-5" href="?page=ul">Home</a>
-            <?php 
-             
-             $showbtn = true;
-             $page = isset($_GET['page']) ? $_GET['page'] : '';
-             
-              if($page == 'ul')
-              require 'includes/ul.php';
-             ?>
+        <div class="col col-xl-3 d-flex flex-column mt-5">
+            <a class="btn border px-5" href="index.php?page=ul">Home</a>
+            <?php
+
+            $_SESSION["showbtn"] = true;
+
+            $page = isset($_GET['page']) ? $_GET['page'] : '';
+            
+            ?>
         </div>
-        <div class="container mt-5 w-50">
-            <?php  
-                if($page == 'form'){
-                    $filename = 'includes/' . $page . '.html';
-                    $showbtn = false;
-                    if(file_exists($filename)){
-                        include ($filename);
-                    }              
-                };
-                if($showbtn){
-                    echo "<a class='btn border px-3 bg-primary text-light' href='?page=form' name='data'>Ajouter des données</a>";
+        <div class="container mt-5 w-md-50 w-sm-75">
+            <?php
+
+
+            if ($page == 'form') {
+                $filename = 'includes/' . $page . '.html';
+                $_SESSION["showbtn"] = false;
+                if (file_exists($filename)) {
+                    include($filename);
                 }
-                ?>
+            };
+
+            if ($_SESSION["showbtn"]) {
+                echo "<a class='btn border px-3 bg-primary text-light' href='?page=form' name='data'>Ajouter des données</a>";
+            }
+
+
+            ?>
         </div>
     </div>
 
-    <?php 
-        include 'includes/footer.html'
+    <?php
+    include 'includes/footer.html'
     ?>
 </body>
+
 </html>
